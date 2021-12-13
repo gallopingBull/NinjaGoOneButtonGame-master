73c3b2e170885f890869161a95795c30ccb90593
Joshua <gallopingbull89@gmail.com>
2021-12-13 09:27:33 -0800

    created final floating island

---
+++ LegoNinjaGo/Assets/LEGO/Scripts/GroundHazard.cs
@@ -0,0 +1,33 @@
+﻿using Unity.LEGO.Game;
+using LEGOMaterials;
+using LEGOModelImporter;
+using System.Collections.Generic;
+using Unity.LEGO.Behaviours.Triggers;
+    
+using Unity.LEGO.Minifig;
+using UnityEngine;
+
+namespace Unity.LEGO.Gameplay
+{
+    public class GroundHazard : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider other) 
+        {
+            if(other.gameObject.CompareTag("Player"))
+            {
+                var minifigController = other.GetComponent<MinifigController>();
+                if (minifigController)
+                {
+                    minifigController.Explode();
+                }
+                
+
+
+                Debug.Log("player hitting");
+                GameOverEvent evt = Events.GameOverEvent;
+                evt.Win = false;
+                EventManager.Broadcast(evt);
+            }
+        }
+    }
+}
